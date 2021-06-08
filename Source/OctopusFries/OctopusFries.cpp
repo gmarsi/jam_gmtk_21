616b94b0f67e82f80b20e6c215916a7df4f5d30a
Geoffrey Marsi <glmarsi@gmail.com>
2021-06-07 20:57:28 -0700

    Starter Unreal project
    Starter content, no template

---
+++ Source/OctopusFries/OctopusFries.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "OctopusFries.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, OctopusFries, "OctopusFries" );
---
+++ Source/OctopusFries/OctopusFriesGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OctopusFriesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OCTOPUSFRIES_API AOctopusFriesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
